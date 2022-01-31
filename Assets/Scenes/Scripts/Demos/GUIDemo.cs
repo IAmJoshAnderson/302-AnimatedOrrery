9e5ec432ed13154a035e08b44e421efdc66ce771
Josh Anderson <anderj93@ferris.edu>
2022-01-31 18:09:40 -0500

    Changing the Camera Views
    It took way too long for me to figure out how to switch between cameras, now to configure the cameras so they don't phase through each other.

--- Assets/Scenes/Scripts/Demos/GUIDemo.cs
+++ Assets/Scenes/Scripts/Demos/GUIDemo.cs
@@ -11,27 +11,93 @@ public class GUIDemo : MonoBehaviour
 
     public Slider slider;
 
+    public GameObject MainCamera;
+    public GameObject camera1;
+    public GameObject camera2;
+    public GameObject camera3;
+    public GameObject camera4;
+    public GameObject camera5;
+
 
     // Start is called before the first frame update
     void Start()
     {
         if(slider) slider.value = Time.timeScale;
+        MainCamera.SetActive(true);
+        camera1.SetActive(false);
+        camera2.SetActive(false);
+        camera3.SetActive(false);
+        camera4.SetActive(false);
+        camera5.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        textPlayerHealth.text = "hello?????";
+
     }
 
-    public void ButtonClicked()
+    public void BackToMainCamera()
     {
-        print("Button Clicked!");
+        MainCamera.SetActive(true);
+        camera1.SetActive(false);
+        camera2.SetActive(false);
+        camera3.SetActive(false);
+        camera4.SetActive(false);
+        camera5.SetActive(false);
+    }
+
+    public void EnableCamera1()
+    {
+        MainCamera.SetActive(false);
+        camera1.SetActive(true);
+        camera2.SetActive(false);
+        camera3.SetActive(false);
+        camera4.SetActive(false);
+        camera5.SetActive(false);
+    }
+
+    public void EnableCamera2()
+    {
+        MainCamera.SetActive(false);
+        camera1.SetActive(false);
+        camera2.SetActive(true);
+        camera3.SetActive(false);
+        camera4.SetActive(false);
+        camera5.SetActive(false);
+    }
+    public void EnableCamera3()
+    {
+        MainCamera.SetActive(false);
+        camera1.SetActive(false);
+        camera2.SetActive(false);
+        camera3.SetActive(true);
+        camera4.SetActive(false);
+        camera5.SetActive(false);
+    }
+    public void EnableCamera4()
+    {
+        MainCamera.SetActive(false);
+        camera1.SetActive(false);
+        camera2.SetActive(false);
+        camera3.SetActive(false);
+        camera4.SetActive(true);
+        camera5.SetActive(false);
+    }
+    public void EnableCamera5()
+    {
+        MainCamera.SetActive(false);
+        camera1.SetActive(false);
+        camera2.SetActive(false);
+        camera3.SetActive(false);
+        camera4.SetActive(false);
+        camera5.SetActive(true);
     }
     public void SliderUpdated(float value)
     {
         Time.timeScale = value;
 
     }
+
 
 }
